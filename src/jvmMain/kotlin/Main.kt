aee229ce2295de148a74c25f6d6234f2e80fdcae
lloydnas <0239559@up.edu.mx>
2022-09-20 13:59:42 -0500

    First layout version

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,32 +1,120 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.ComposeWindow
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowState
+import javax.swing.JFileChooser
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var flashFilePicker = java.awt.FileDialog(ComposeWindow())
+    var eepromFilePicker = java.awt.FileDialog(ComposeWindow())
+
+    var flashFile = remember { mutableStateOf("") }
+    var eepromFile = remember { mutableStateOf("") }
+
+    var highFuseValue = remember { mutableStateOf("") }
+    var lowFuseValue = remember { mutableStateOf("") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Row(Modifier.fillMaxSize(), Arrangement.SpaceEvenly, Alignment.CenterVertically) {
+            Column(Modifier.wrapContentSize(),
+                Arrangement.spacedBy(50.dp)){
+
+                Column(Modifier.wrapContentSize(),
+                Arrangement.spacedBy(5.dp),
+                    Alignment.CenterHorizontally){
+                    Text(text = "write on FLASH memory")
+                    Button(onClick = {
+                        flashFilePicker.isVisible = true
+                        flashFile.value = flashFilePicker.file
+                    }){
+                        Text(if(flashFile.value.isEmpty()) "Select a .hex file..." else flashFile.value)
+                    }
+                    Button(onClick = {},
+                        Modifier.align(Alignment.CenterHorizontally)){
+                        Text("Write")
+                    }
+                }
+
+                Column(Modifier.wrapContentSize(),
+                    Arrangement.spacedBy(5.dp)){
+                    Text(text = "write on EEPROM memory")
+                    Button(onClick = {
+                        eepromFilePicker.isVisible = true
+                        eepromFile.value = eepromFilePicker.file
+                    }){
+                        Text(if(eepromFile.value.isEmpty()) "Select a .hex file..." else eepromFile.value)
+                    }
+                    Button(onClick = {},
+                    Modifier.align(Alignment.CenterHorizontally)){
+                        Text("Write")
+                    }
+                }
+            }
+
+            Column(Modifier.wrapContentSize(),
+            Arrangement.spacedBy(5.dp),
+            Alignment.CenterHorizontally) {
+                Row(Modifier.wrapContentSize(),
+                Arrangement.spacedBy(2.dp)){
+                    Text(text = "High Fuse")
+                    TextField(
+                        value = highFuseValue.value,
+                        onValueChange = {highFuseValue.value = it},
+                        modifier = Modifier.wrapContentSize()
+                            .clip(RoundedCornerShape(100.dp))
+
+                    )
+                }
+
+                Row(Modifier.wrapContentSize(),
+                    Arrangement.spacedBy(2.dp)){
+                    Text(text = "Low Fuse")
+                    TextField(
+                        value = lowFuseValue.value,
+                        onValueChange = {lowFuseValue.value = it},
+                        modifier = Modifier.wrapContentSize()
+                    )
+                }
+
+                Row(Modifier.wrapContentSize(),
+                    Arrangement.spacedBy(2.dp)){
+                    Button(onClick = {}){
+                        Text("Read")
+                    }
+
+                    Button(onClick = {}){
+                        Text("Write")
+                    }
+                }
+
+            }
+
         }
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(onCloseRequest = ::exitApplication,
+    title = "sUPer Burner",
+    state = WindowState(position = WindowPosition(Alignment.Center))) {
         App()
     }
 }
